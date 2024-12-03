679666c5dfd2a530d62e76268b8f19447f3aef77
GSRadev21 <GSRadev21@codingburgas.bg>
2024-12-03 12:04:43 +0200

    Repair

--- 3LArcModel/src/main/java/PresentationLayer/example/Main.java
+++ 3LArcModel/src/main/java/PresentationLayer/example/Main.java
@@ -1,11 +1,12 @@
-package org.example;
+package PresentationLayer.example;
 
 import PresentationLayer.Config.AppConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import PresentationLayer.UserController;
 
 public class Main {
-    public static <UserController> void main(String[] args) {
+    public static <Controller> void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         UserController userController = context.getBean(UserController.class);
         userController.run();
